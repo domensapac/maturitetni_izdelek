b6e286c6229010f761687d93e994e495ec71ba3f
dddomennn <domen.sapac420@gmail.com>
2025-02-25 20:18:35 +0100

    login better

--- maturitetni_izdelek/resources/views/login.blade.php
+++ maturitetni_izdelek/resources/views/login.blade.php
@@ -53,10 +53,8 @@
             </div>
             <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
               <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                <h4 class="mb-4">We are more than just a company</h4>
-                <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <h4 class="mb-4">Hitro in enostavno do malice!</h4>
+                <p class="small mb-0">Naša aplikacija omogoča učencem enostaven prevzem šolske malice. Preprosto skenirajte svojo QR kodo in prevzemite obrok brez zamud. S tem poskrbimo za hitrejši potek delitve malice ter večjo preglednost in učinkovitost. Prijavite se in uživajte!</p>
               </div>
             </div>
           </div>
