7cd373b6b5344c70e7c29754a0b6d78fcbd68259
dddomennn <domen.sapac420@gmail.com>
2025-03-17 16:49:05 +0100

    welcome page

--- maturitetni_izdelek/resources/views/welcome.blade.php
+++ maturitetni_izdelek/resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        
     </head>
 <body>
 
@@ -21,7 +22,10 @@
 <section>
 <div class="displayed_content">
 
+    <p class="user_name"> {{ Auth::user()->name ?? 'Gost' }} {{ Auth::user()->surname ?? '' }} </p>
+
     <div class="qr_container">
+
         <div class="qr_pic">
             {!! $qrCode !!} 
         </div>
@@ -29,6 +33,35 @@
 </div>
 </section>
 
+<section class="other_content">
+
+    <div class="content_row">
+        <h1>Kako do malice?</h1>
+    </div>
+   
+    <div class="content_row">
+        <div class="text_box">
+            <h2>Poskeniraj kodo</h2>
+            <p>
+            Pripravite aplikacijo z vašo QR kodo in jo usmerite proti skenerju. Pomembno je, da je koda jasno vidna in v celoti zajeta v okviru kamere. Ko bo koda uspešno prepoznana, boste prejeli potrditev o skeniranju.            </p>
+        </div>
+        <div class="image_box">
+            <img src="{{ asset('images/app.png') }}" alt="Slika aplikacije">
+        </div>
+    </div>
+
+    <div class="content_row reverse">
+        <div class="image_box">
+            <img src="{{ asset('images/success.png') }}" alt="Slika uspeh">
+        </div>
+        <div class="text_box">
+            <h2>Prejmi malico</h2>
+            <p>
+            Ko je vaša QR koda uspešno skenirana, bo sistem preveril vašo upravičenost do obroka. Nato lahko prevzamete svojo malico hitro in brez nepotrebnega čakanja. Poskrbite, da bo vaša koda vedno pravilno usmerjena in vidna, da preprečite morebitne težave pri skeniranju.
+            </p>
+        </div>
+    </div>
+</section>
 
 
 <section>
